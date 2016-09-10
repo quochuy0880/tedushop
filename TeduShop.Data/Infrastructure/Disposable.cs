023d53a6957b9270bf21fd71dfcb9cbf0283e6de
DESKTOP-16KHEML\JASON <quochuy0880@gmail.com>
2016-09-10 09:14:50 +0700

    Update Data

---
+++ TeduShop.Data/Infrastructure/Disposable.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TeduShop.Data.Infrastructure
+{
+    public class Disposable : IDisposable
+    {
+        public bool isDisposed;
+        ~Disposable()
+        {
+            Dispose(false);
+        }
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);//dung de thu hoi lai bo nho
+        }
+        private void Dispose(bool disposing)
+        {
+            if (!isDisposed && disposing)
+            {
+                DisposeCore();
+            }
+
+            isDisposed = true;
+        }
+        //Override this to dispose customer objects
+        protected virtual void DisposeCore()
+        { }
+    }
+}
