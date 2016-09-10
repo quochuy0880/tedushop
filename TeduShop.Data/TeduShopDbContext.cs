7049811ee52961ba04184f5575496fce7ca7c6f0
DESKTOP-16KHEML\JASON <quochuy0880@gmail.com>
2016-09-10 16:36:01 +0700

    Lession 12

--- TeduShop.Data/TeduShopDbContext.cs
+++ TeduShop.Data/TeduShopDbContext.cs
@@ -31,6 +31,7 @@ namespace TeduShop.Data
         public DbSet<SystemConfig> SystemConfigs { set; get; }
         public DbSet<Tag> Tags { set; get; }
         public DbSet<VisitorStatistic> VisitorStatistics { set; get; }
+        public DbSet<Error> Errors { set; get; }
 
         protected override void OnModelCreating(DbModelBuilder builder)
         {
