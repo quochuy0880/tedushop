dd62627d5b3e0ce201fafa7124ba955b0e1edb05
DESKTOP-16KHEML\JASON <quochuy0880@gmail.com>
2016-09-10 11:43:58 +0700

    Add more lession 7

---
+++ TeduShop.Data/Repositories/OrderDetailRepository.cs
@@ -0,0 +1,14 @@
+﻿using TeduShop.Data.Infrastructure;
+using TeduShop.Model.Models;
+
+namespace TeduShop.Data.Repositories
+{
+    public interface IOrderDetailRepository : IRepository<OrderDetail>
+    { }
+
+    public class OrderDetailRepository : RepositoryBase<OrderDetail>, IOrderDetailRepository
+    {
+        public OrderDetailRepository(IDbFactory dbFactory) : base(dbFactory)
+        { }
+    }
+}
---
+++ TeduShop.Data/Repositories/PageRepository.cs
@@ -0,0 +1,14 @@
+﻿using TeduShop.Data.Infrastructure;
+using TeduShop.Model.Models;
+
+namespace TeduShop.Data.Repositories
+{
+    public interface IPageRepository : IRepository<Page>
+    { }
+
+    public class PageRepository : RepositoryBase<Page>, IPageRepository
+    {
+        public PageRepository(IDbFactory dbFactory) : base(dbFactory)
+        { }
+    }
+}
