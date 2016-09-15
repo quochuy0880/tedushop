ddf69bd4d11f647cb975d8ae30e4e3fe0d28d1d5
DESKTOP-16KHEML\JASON <quochuy0880@gmail.com>
2016-09-15 15:14:39 +0700

    Filter status and format data in list

---
+++ TeduShop.Web/app/shared/filters/statusFilter.js
@@ -0,0 +1,10 @@
+﻿(function (app) {
+    app.filter('statusFilter', function () {
+        return function (input) {
+            if (input == true)
+                return 'Actived';
+            else
+                return 'Deactivated';
+        }
+    });
+})(angular.module('tedushop.common'));
